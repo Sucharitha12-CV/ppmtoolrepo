bb95acbc262d8fc75c9dcd54e7fa8dd72bae9ae5
Sharma Pankaj <sharma.pankaj@Yash.com>
2020-04-20 12:51:36 +0530

    PPMCLI-Day-3 (Redux): Create Project Logic Added

--- ppmcli/src/App.js
+++ ppmcli/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import React, { Component } from 'react'
-import Dashboard from './components/Dashboard';
-import Header from './components/layout/Header';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import AddProject from './components/projects/AddProject';
+import React, { Component } from "react";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/projects/AddProject";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Header/>
-        <Route path="/dashboard" component={Dashboard}/>
-        <Route path="/addProject" component={AddProject}/>
+      <Provider store={store}>
+        <Router>
+          <Header />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/addProject" component={AddProject} />
         </Router>
-    )
+      </Provider>
+    );
   }
 }
 
 export default App;
-
