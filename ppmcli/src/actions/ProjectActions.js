f2138fa73abbf2337d5c2b73a25a40a3ee6481b3
Sharma Pankaj <sharma.pankaj@Yash.com>
2020-05-15 22:41:20 +0530

    PPMCLI-Day-4 (proxy and delele): proxy and safe delete implemented

--- ppmcli/src/actions/ProjectActions.js
+++ ppmcli/src/actions/ProjectActions.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./type";
 export const createProject = (project, history) => async dispatch => {
   try {
-    const res = await axios.post("http://localhost:8080/api/project", project);
+    const res = await axios.post("/api/project", project);
     history.push("/dashboard");
   } catch (error) {
     dispatch({
@@ -13,14 +13,14 @@ export const createProject = (project, history) => async dispatch => {
 };
 
 export const getProjects = () => async dispatch => {
-  const res = await axios.get("http://localhost:8080/api/project/all");
+  const res = await axios.get("/api/project/all");
   dispatch({
     type: GET_PROJECTS,
     payload: res.data
   });
 };
 export const getProject = (id, history) => async dispatch => {
-  const res = await axios.get(`http://localhost:8080/api/project/${id}`);
+  const res = await axios.get(`/api/project/${id}`);
   dispatch({
     type: GET_PROJECT,
     payload: res.data
@@ -28,9 +28,15 @@ export const getProject = (id, history) => async dispatch => {
 };
 
 export const deleteProject = id => async dispatch => {
-  await axios.delete(`http://localhost:8080/api/project/${id}`);
-  dispatch({
-    type: DELETE_PROJECT,
-    payload: id
-  });
+  if (
+    window.confirm(
+      "Are you sure? This will delete the project and the data related to it"
+    )
+  ) {
+    await axios.delete(`/api/project/${id}`);
+    dispatch({
+      type: DELETE_PROJECT,
+      payload: id
+    });
+  }
 };
