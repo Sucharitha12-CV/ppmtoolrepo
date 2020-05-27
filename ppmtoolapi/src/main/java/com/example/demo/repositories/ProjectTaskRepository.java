0f7f97ecebbc1f29194e630f3b627e5b316bac05
Sharma Pankaj <sharma.pankaj@Yash.com>
2020-05-27 10:17:47 +0530

    PPMCLI- (ProjectTask): Get ProjectTask by projectSequence

--- ppmtoolapi/src/main/java/com/example/demo/repositories/ProjectTaskRepository.java
+++ ppmtoolapi/src/main/java/com/example/demo/repositories/ProjectTaskRepository.java
@@ -11,4 +11,6 @@ import com.example.demo.domain.ProjectTask;
 public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long>{
 
 	List<ProjectTask> findByProjectIdentifierOrderByPriority(String id);
+	
+	ProjectTask findByProjectSequence(String sequence);
 }
