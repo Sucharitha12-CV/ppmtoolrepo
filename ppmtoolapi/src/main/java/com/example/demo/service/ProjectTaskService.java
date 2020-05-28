26ea137a03c577232379127b0a89efba18001a93
Sharma Pankaj <sharma.pankaj@Yash.com>
2020-05-28 18:19:09 +0530

    PPMCLI- (ProjectTask): Update ProjTask-Improvement, Delete ProjTask

--- ppmtoolapi/src/main/java/com/example/demo/service/ProjectTaskService.java
+++ ppmtoolapi/src/main/java/com/example/demo/service/ProjectTaskService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -88,11 +90,20 @@ public class ProjectTaskService {
 	
 	public ProjectTask updateByProjectSequence(ProjectTask updatedTask, String backlog_id, String pt_id) {
 		//Find existing project task
-		ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
+		ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
 		//Replace it with updated task
 		projectTask = updatedTask;
 		//Save projectTask
 		return projectTaskRepository.save(projectTask);
+	}
+	
+	public void deletePTByProjectSequence(String backlog_id,String pt_id) {
+		ProjectTask projectTask =  findPTByProjectSequence(backlog_id, pt_id);
+		Backlog backlog = projectTask.getBacklog();
+		List<ProjectTask> pts = backlog.getProjectTasks();
+		pts.remove(projectTask);
+		backlogRepository.save(backlog);
+		projectTaskRepository.delete(projectTask);
 	}
 
 }
