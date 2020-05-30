72f065411bc442179fd0e7190ba3adda5659cb72
Sharma Pankaj <sharma.pankaj@Yash.com>
2020-05-30 07:37:02 +0530

    PPMCLI- (ProjectTask): ProjectBoard Route, Add Project Task

--- ppmcli/src/App.js
+++ ppmcli/src/App.js
@@ -10,6 +10,8 @@ import AddProject from "./components/projects/AddProject";
 import { Provider } from "react-redux";
 import store from "./store";
 import UpdateProject from "./components/projects/UpdateProject";
+import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
+import AddProjectTask from "./components/ProjectBoard/ProjectTasks/AddProjectTask";
 
 class App extends Component {
   render() {
@@ -17,9 +19,11 @@ class App extends Component {
       <Provider store={store}>
         <Router>
           <Header />
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="/addProject" component={AddProject} />
-          <Route path="/updateProject/:id" component={UpdateProject} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+          <Route exact path="/updateProject/:id" component={UpdateProject} />
+          <Route exact path="/projectBoard/:id" component={ProjectBoard} />
+          <Route exact path="/addProjectTask/:id" component={AddProjectTask} />
         </Router>
       </Provider>
     );
--- ppmcli/src/components/ProjectItem.js
+++ ppmcli/src/components/ProjectItem.js
@@ -6,7 +6,7 @@ import { PropTypes } from "prop-types";
 import { deleteProject } from "../../src/actions/ProjectActions";
 
 class ProjectItem extends Component {
-  onDeleteClick = id => {
+  onDeleteClick = (id) => {
     console.log("-------> method called.");
     this.props.deleteProject(id);
   };
@@ -27,11 +27,12 @@ class ProjectItem extends Component {
             </div>
             <div className="col-md-4 d-none d-lg-block">
               <ul className="list-group">
-                <a href="#">
+                <Link to={`/projectBoard/${project.projectIdentifier}`}>
                   <li className="list-group-item board">
                     <i className="fa fa-flag-checkered pr-1"> Project Board </i>
                   </li>
-                </a>
+                </Link>
+
                 <Link to={`/updateProject/${project.projectIdentifier}`}>
                   <li className="list-group-item update">
                     <i className="fa fa-edit pr-1"> Update Project Info</i>
@@ -57,7 +58,7 @@ class ProjectItem extends Component {
 }
 
 ProjectItem.propTypes = {
-  deleteProject: PropTypes.func.isRequired
+  deleteProject: PropTypes.func.isRequired,
 };
 
 export default connect(
